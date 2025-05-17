a6b5b8d1dac0ac2823ba6247da25a5a666524bec
LittleSlimeBoi <97430502+LittleSlimeBoi@users.noreply.github.com>
2025-05-17 14:29:00 +0700

    lab3.1 ky 2024.2

---
+++ STM32F429I_DISCO_REV_D01/Debug/Application/User/TouchGFX/App/app_touchgfx.d
@@ -0,0 +1,4 @@
+Application/User/TouchGFX/App/app_touchgfx.o: \
+ D:/TouchGFX/TouchGFXProjects/HelloTouchGFX/TouchGFX/App/app_touchgfx.c \
+ D:/TouchGFX/TouchGFXProjects/HelloTouchGFX/TouchGFX/App/app_touchgfx.h
+D:/TouchGFX/TouchGFXProjects/HelloTouchGFX/TouchGFX/App/app_touchgfx.h:
---
+++ STM32F429I_DISCO_REV_D01/Debug/Drivers/BSP/Components/stmpe811.d
@@ -0,0 +1,8 @@
+Drivers/BSP/Components/stmpe811.o: \
+ D:/TouchGFX/TouchGFXProjects/HelloTouchGFX/Drivers/BSP/Components/stmpe811/stmpe811.c \
+ D:/TouchGFX/TouchGFXProjects/HelloTouchGFX/Drivers/BSP/Components/stmpe811/stmpe811.h \
+ D:/TouchGFX/TouchGFXProjects/HelloTouchGFX/Drivers/BSP/Components/stmpe811/../Common/ts.h \
+ D:/TouchGFX/TouchGFXProjects/HelloTouchGFX/Drivers/BSP/Components/stmpe811/../Common/io.h
+D:/TouchGFX/TouchGFXProjects/HelloTouchGFX/Drivers/BSP/Components/stmpe811/stmpe811.h:
+D:/TouchGFX/TouchGFXProjects/HelloTouchGFX/Drivers/BSP/Components/stmpe811/../Common/ts.h:
+D:/TouchGFX/TouchGFXProjects/HelloTouchGFX/Drivers/BSP/Components/stmpe811/../Common/io.h:
